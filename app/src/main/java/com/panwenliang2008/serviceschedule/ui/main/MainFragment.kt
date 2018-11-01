5325f1df2a5dd0eedb6bcfa8f68d995be979c656
buliang <panwenliang2008@gmail.com>
2018-11-01 22:49:09 +0900

    init

--- app/src/main/java/com/panwenliang2008/serviceschedule/ui/main/MainFragment.kt
+++ app/src/main/java/com/panwenliang2008/serviceschedule/ui/main/MainFragment.kt
@@ -1,5 +1,6 @@
 package com.panwenliang2008.serviceschedule.ui.main
 
+import android.arch.lifecycle.Observer
 import android.arch.lifecycle.ViewModelProviders
 import android.os.Bundle
 import android.support.v4.app.Fragment
@@ -33,7 +34,9 @@ class MainFragment : BaseFragment() {
         viewModel = ViewModelProviders.of(this).get(MainViewModel::class.java)
         // TODO: Use the ViewModel
 
-        tvTimeDate.text = TimeUtils.getNowString()
+        viewModel.currentTime.observe(this, Observer {
+            tvTimeDate.text = it
+        })
     }
 
 }
--- app/src/main/java/com/panwenliang2008/serviceschedule/ui/main/MainViewModel.kt
+++ app/src/main/java/com/panwenliang2008/serviceschedule/ui/main/MainViewModel.kt
@@ -1,7 +1,35 @@
 package com.panwenliang2008.serviceschedule.ui.main
 
+import android.arch.lifecycle.LiveData
+import android.arch.lifecycle.MutableLiveData
+import android.arch.lifecycle.Transformations
 import android.arch.lifecycle.ViewModel
+import android.content.Intent
+import android.os.Handler
+import android.view.Menu
+import com.blankj.utilcode.util.TimeUtils
+import com.blankj.utilcode.util.TimeUtils.getNowString
+import com.blankj.utilcode.util.Utils
+import java.util.*
+import java.util.concurrent.TimeUnit
 
 class MainViewModel : ViewModel() {
     // TODO: Implement the ViewModel
+
+    private val _currentTime : MutableLiveData<String> = MutableLiveData()
+
+    val currentTime : LiveData<String>
+    get() = _currentTime
+
+    private val timeHandler = Handler()
+    private val timerRunnable = object : Runnable {
+        override fun run() {
+            _currentTime.value = TimeUtils.getNowString()
+            timeHandler.postDelayed(this, TimeUnit.SECONDS.toMillis(1))
+        }
+    }
+
+    init {
+        timeHandler.post(timerRunnable)
+    }
 }
